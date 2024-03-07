1c928be5af4d8511d73eb5f237a547c8123d9637
luan-nee <luandelsol54@gmail.com>
2024-03-06 19:40:30 -0500

    changes

--- main.js
+++ main.js
@@ -17,7 +17,22 @@ function getCuadrado({color = 'white' , x = 0}){
     width: 10,
     height: 30,
     color: color,
+    direction: 'left',
     draw () {
+      if(this.direction === 'left'){
+        this.x--;
+      }
+      if(this.direction === 'right'){
+        this.x++;
+      }
+
+      if(this.x == canvas.width){
+        this.direction = 'left'; 
+      }
+      if(this.x == 0){
+        this.direction = 'right';
+      }
+
       ctx.fillStyle = this.color;
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
@@ -27,5 +42,17 @@ function getCuadrado({color = 'white' , x = 0}){
 const casa1 = getCuadrado({color: 'yellow', x: 10});
 casa1.draw();
 
-const casa2 = getCuadrado({color: 'green', x: canvas.width-20 })
+const casa2 = getCuadrado({color: 'green', x: canvas.width-20 });
 casa2.draw();
+
+const ball = getCuadrado({color: 'red', x: canvas.width/2});
+
+function frame(){
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  ball.draw();
+  console.log('dibujando');
+  requestAnimationFrame(frame);
+}
+
+//pasale una función que se repirá constantemente
+requestAnimationFrame(frame);
